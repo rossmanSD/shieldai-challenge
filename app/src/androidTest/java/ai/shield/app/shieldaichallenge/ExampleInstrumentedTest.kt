2c99299702faba00b597370153e3422c6741ec83
Ross Manges <ross@sparkistic.com>
2022-06-06 18:14:04 -0700

    Adding the first instrumented test.

--- app/src/androidTest/java/ai/shield/app/shieldaichallenge/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/ai/shield/app/shieldaichallenge/ExampleInstrumentedTest.kt
@@ -1,24 +1,54 @@
 package ai.shield.app.shieldaichallenge
 
-import androidx.test.platform.app.InstrumentationRegistry
+import ai.shield.app.shieldaichallenge.view.MainActivity
+import androidx.recyclerview.widget.RecyclerView
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.contrib.RecyclerViewActions
+import androidx.test.espresso.contrib.RecyclerViewActions.scrollToPosition
+import androidx.test.espresso.matcher.ViewMatchers.*
+import androidx.test.ext.junit.rules.activityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
-
+import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 
-import org.junit.Assert.*
 
-/**
- * Instrumented test, which will execute on an Android device.
- *
- * See [testing documentation](http://d.android.com/tools/testing).
- */
 @RunWith(AndroidJUnit4::class)
 class ExampleInstrumentedTest {
+    @get:Rule
+    val rule = activityScenarioRule<MainActivity>()
+
+
+
     @Test
-    fun useAppContext() {
-        // Context of the app under test.
-        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("ai.shield.app.shieldaichallenge", appContext.packageName)
+    fun verifyTitleOfS1Ep3() {
+        onView(withId(R.id.recycler_view))
+            .perform(scrollToPosition<RecyclerView.ViewHolder>(2))
+            .check(
+                matches(
+                    Utils.atPosition(2,
+                        hasDescendant(
+                            withText("S1 Ep3: Lord Snow")
+                        )
+                    )
+                )
+            )
+
+        // click on the item in the recycleview to load details
+        onView(withId(R.id.recycler_view)).perform(
+            RecyclerViewActions.actionOnItemAtPosition<RecyclerView.ViewHolder>(
+                2,
+                click()
+            )
+        )
+
+        // now check details pane
+        onView(withId(R.id.description)).check(
+            matches(
+                    withText("Jon Snow attempts to find his place amongst the Night's Watch. Eddard and his daughters arrive at King's Landing.\n\n")
+            )
+        )
     }
 }
